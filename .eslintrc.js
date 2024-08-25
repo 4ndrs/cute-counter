d469354bb5eea75f0c664fe553ae43308ec1cca5
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2024-08-25 02:36:25 +0200

    feat: add counter

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,5 @@
 module.exports = {
   extends: ["expo", "prettier", "plugin:tailwindcss/recommended"],
   plugins: ["prettier"],
-  rules: {
-    "prettier/prettier": "warn",
-  },
+  rules: {},
 };
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,32 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
+import { SafeAreaView, View } from "react-native";
+import { Pressable, Text } from "react-native";
 
 const Home = () => {
+  const [count, setCount] = useState(0);
+
   return (
-    <SafeAreaView>
-      <Text className="text-center text-2xl font-bold text-blue-600">
-        Home Screen
-      </Text>
+    <SafeAreaView className="flex-1 items-center justify-center">
+      <View className="items-center">
+        <Text
+          adjustsFontSizeToFit
+          numberOfLines={1}
+          className="text-center font-[Menlo] text-[200px] font-bold text-blue-400"
+        >
+          {count}
+        </Text>
+
+        <Pressable
+          onPress={() => setCount(count + 1)}
+          className="size-40 items-center justify-center rounded-full border-[6px] border-blue-300 bg-blue-400 active:bg-blue-500"
+        >
+          <Text className="text-2xl font-bold text-white">Touch</Text>
+        </Pressable>
+
+        <Pressable onPress={() => setCount(0)} className="mt-10">
+          <Text className="text-lg font-medium text-red-500">Reset</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 };
