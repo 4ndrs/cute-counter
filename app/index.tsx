27c46c84049075f36fa23d18ebb8c420f629ac6d
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2024-08-25 03:59:49 +0200

    feat: add haptics + reset button alert

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
-import { SafeAreaView, View } from "react-native";
 import { Pressable, Text } from "react-native";
+import { Alert, SafeAreaView, View } from "react-native";
+
+import * as Haptics from "expo-haptics";
 
 const Home = () => {
   const [count, setCount] = useState(0);
@@ -17,13 +19,45 @@ const Home = () => {
         </Text>
 
         <Pressable
-          onPress={() => setCount(count + 1)}
+          onPress={() => {
+            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Soft);
+            setCount(count + 1);
+          }}
           className="size-40 items-center justify-center rounded-full border-[6px] border-blue-300 bg-blue-400 active:bg-blue-500"
         >
           <Text className="text-2xl font-bold text-white">Touch</Text>
         </Pressable>
 
-        <Pressable onPress={() => setCount(0)} className="mt-10">
+        <Pressable
+          onPress={() => {
+            if (count === 0) {
+              return;
+            }
+
+            Haptics.notificationAsync(Haptics.NotificationFeedbackType.Warning);
+            Alert.alert(
+              "Reset",
+              "Are you sure you want to reset the counter?",
+              [
+                {
+                  text: "Cancel",
+                  style: "cancel",
+                },
+                {
+                  text: "OK",
+                  style: "destructive",
+                  onPress: () => {
+                    Haptics.notificationAsync(
+                      Haptics.NotificationFeedbackType.Success,
+                    );
+                    setCount(0);
+                  },
+                },
+              ],
+            );
+          }}
+          className="mt-10"
+        >
           <Text className="text-lg font-medium text-red-500">Reset</Text>
         </Pressable>
       </View>
